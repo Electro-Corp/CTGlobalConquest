1545821b0c9651893fd4670324050274725fca85
Androo <andrew.coon84@gmail.com>
2022-12-01 19:54:30 -0700

    Figured out he issue

--- src/main/java/gd/rf/jsgames/App.java
+++ src/main/java/gd/rf/jsgames/App.java
@@ -9,33 +9,19 @@ import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.GameWorld;
 import com.almasb.fxgl.input.Input;
 import com.almasb.fxgl.input.InputModifier;
-import com.almasb.fxgl.input.InputSequence;
 import com.almasb.fxgl.input.UserAction;
 
-import gd.rf.jsgames.civics.civics;
 import gd.rf.jsgames.datatypes.Point;
 import gd.rf.jsgames.tiles.Grass;
 import gd.rf.jsgames.tiles.Tile;
 import gd.rf.jsgames.ui.ui;
 import gd.rf.jsgames.units.Settler;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.scene.Node;
-import javafx.scene.control.TextInputDialog;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-import com.almasb.fxgl.ui.*;
 import static com.almasb.fxgl.dsl.FXGL.*;
-import java.io.FileNotFoundException;
 import java.util.*;
-
-import java.util.Map;
 
 //https://github.com/AlmasB/FXGL/issues/526
 public class App extends GameApplication {
@@ -59,6 +45,8 @@ public class App extends GameApplication {
     private ArrayList<Tile> sTiles;
     ui mainUI;
 
+    private static Entity[][] tileEntities;
+
     @Override
     protected void initSettings(GameSettings settings) {
         settings.setWidth(1024);
@@ -76,12 +64,12 @@ public class App extends GameApplication {
     protected void initGame() {
         gw = getGameWorld();
         om = new ObjectManager();
-
+        tileEntities = new Entity[BOARD_X][BOARD_Y];
         getGameScene().setBackgroundColor(Color.BLACK);
         om.units.add(new Settler(0, 3));
         om.units.get(0).moveTo(6, 5);
         // createBoard();
-        
+
         mainUI = new ui(this);
         for (int i = 0; i < TILE_COUNT_Y; i++) {
             for (int j = 0; j < TILE_COUNT_X; j++) {
@@ -91,7 +79,6 @@ public class App extends GameApplication {
 
         getGameScene().addUINode(mainUI);
         initUI();
-
     }
 
     protected void initUI() {
@@ -116,8 +103,10 @@ public class App extends GameApplication {
                 double x = tiles[j][i].x;
                 double y = tiles[j][i].y;
                 String path = tiles[j][i].path;
+                Node node = FXGL.getAssetLoader().loadTexture(path);
                 tileEntities[j][i].setPosition(x, y);
-                tileEntities[j][i].setVie(path);
+                // tileEntities[j][i].getViewComponent().clearChildren();
+                // tileEntities[j][i].getViewComponent().addChild(node);
 
                 // gw.addEntity(tileEntities[j][i]);
                 // System.out.println(tiles[j][i]);
@@ -151,7 +140,7 @@ public class App extends GameApplication {
                 clearSTiles();
                 sTiles.clear();
                 int dy = (int) (input.getMouseYWorld() + MOUSE_OFFSET.y) / (TILE_SIZE + BORDER_WIDTH);
-                
+
                 int dx = (int) (input.getMouseXWorld() + MOUSE_OFFSET.x) / (TILE_SIZE + BORDER_WIDTH);
                 try {
                     cTile.changeSelected();
@@ -230,9 +219,9 @@ public class App extends GameApplication {
         for (int i = 0; i < sTiles.size(); i++) {
             int x = (int) sTiles.get(i).x;
             int y = (int) sTiles.get(i).y;
-            //if (sTiles.get(i).selected) {
-                sTiles.get(i).changeSelected();
-                System.out.println(sTiles.get(i));
+            // if (sTiles.get(i).selected) {
+            sTiles.get(i).changeSelected();
+            System.out.println(sTiles.get(i));
             // }
             tiles[x][y] = sTiles.get(i);
             System.out.println(tiles[x][y]);
